0d2a9bf2c057b222e822b96eb38b6b4253130571
Joseph-Cha <75019048+Joseph-Cha@users.noreply.github.com>
2021-05-17 09:24:41 +0900

    Ch3 3.9 타입 안정성

---
+++ Programming_Practice_Using_Cpp/vs2019/ch3_3.cpp
@@ -0,0 +1,18 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int number_of_words = 0;
+//	string previous = " ";
+//	string current;
+//	while (cin >> current)
+//	{
+//		++number_of_words;
+//		if (previous == current)
+//			cout << "단어 번호" << number_of_words 
+//			<< "반복된 단어: " << current << '\n';
+//		previous = current;
+//	}
+//}
---
+++ Programming_Practice_Using_Cpp/vs2019/ch3_4.cpp
@@ -0,0 +1,8 @@
+//#include <std_lib_facilities.h>
+//using namespace std;
+//
+//int main()
+//{
+//	string s = "잔인한 세상이여, 안녕히!";
+//	cout << s << '\n';
+//}
