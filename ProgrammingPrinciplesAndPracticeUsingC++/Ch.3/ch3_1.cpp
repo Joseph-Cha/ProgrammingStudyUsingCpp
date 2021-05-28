5839b5059dacf34361f99acbf6c54c580ac54a05
Joseph-Cha <75019048+Joseph-Cha@users.noreply.github.com>
2021-05-28 09:41:14 +0900

    파일 이주 및 정리

---
+++ ProgrammingPrinciplesAndPracticeUsingC++/Ch.3/ch3_1.cpp
@@ -0,0 +1,19 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    cout << "부동소수점 값 입력";
+//    int n;
+//    cin >> n;
+//    cout << "n == " << n
+//        << "\nn + 1 == " << n + 1
+//        << "\n3 곱하기 == " << n * 3
+//        << "\nn의 두 배 == " << n + n
+//        << "\nn의 제곱 == " << n * n
+//        << "\nn의 나누기 2 == " << n / 2
+//        << "\nn의 제곱근 == " << sqrt((double)n)
+//        << '\n';
+//}
+//
